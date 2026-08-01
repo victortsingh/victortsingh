c1c479c1da005bf7f66539bb37529e20c124a6b9
victortsingh <victor.t.singh@gmail.com>
2026-08-01 02:34:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -81,8 +81,8 @@ if (carousel) {
   // Edit this list to add/remove/reorder your own photos.
   // Put the image files in assets/photos/ using these filenames (or update the paths below).
   const photos = [
-    { src: 'assets/photos/Photo_website_3.jpg', alt: 'Photo 1' },
-    { src: 'assets/photos/Victor_S_Website_p1.jpeg', alt: 'Photo 2' },
+    { src: 'assets/photos/Victor_S_Website_p1.jpeg', alt: 'Photo 1' },
+    { src: 'assets/photos/Photo_website_3.jpg', alt: 'Photo 2' },
   ];
 
   const img = document.getElementById('carousel-img');
